166c617e190a91ff0c38c95b2209d776da91d7ef
AJAY YADAV <imajydv8298@gmail.com>
2022-12-31 18:29:05 +0530

    counter app completed

--- Level 3/counter-app/src/App.jsx
+++ Level 3/counter-app/src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  
+
+  const [count, setCount] = useState(0)
+
+  function increase() {
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>Count</button>
+    </div>
+  )
 }
 
 export default App

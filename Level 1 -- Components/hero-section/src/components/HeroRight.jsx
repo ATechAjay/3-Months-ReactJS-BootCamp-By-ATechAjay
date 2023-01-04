051b9eca9a399318c49ea9565b7cb55d2a02ae37
AJAY YADAV <imajydv8298@gmail.com>
2023-01-04 12:41:35 +0530

    Modified a little bit

--- Level 1 -- Components/hero-section/src/components/HeroRight.jsx
+++ Level 1 -- Components/hero-section/src/components/HeroRight.jsx
@@ -4,8 +4,8 @@ function HeroRight() {
     <div className="hero__right">
       <img
         className="hero__right--image"
-        src="src\assets\hero-img.jpg"
-        alt=""
+        src="https://juho.co/wp-content/uploads/2022/10/bubbles-eng-1-1024x793.jpg"
+        alt="hero image"
       />
     </div>
   );
--- Level 1 -- Components/hero-section/src/components/Navbar.jsx
+++ Level 1 -- Components/hero-section/src/components/Navbar.jsx
@@ -3,7 +3,11 @@ function Navbar() {
   return (
     <nav className="navbar">
       <a href="#">
-        <img className="navbar__logo" src="src\assets\logo.svg" alt="logo" />
+        <img
+          className="navbar__logo"
+          src="https://juho.co/wp-content/uploads/2021/08/juho-square-dark-1.svg"
+          alt="logo"
+        />
       </a>
 
       <ul className="navbar__links--container">

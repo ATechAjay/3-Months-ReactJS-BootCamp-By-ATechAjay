01bb8b7e4a753b87c4f8794117314f68ea2e6b62
AJAY YADAV <imajydv8298@gmail.com>
2023-01-06 07:33:05 +0530

    Splitting todo list component into date components. date component is now extracted from the existing components.

--- Level 1 -- Components/todo-list/src/TodoListItem.jsx
+++ Level 1 -- Components/todo-list/src/TodoListItem.jsx
@@ -1,12 +1,12 @@
 import "./TodoListItem.css";
-
+import TodoListDate from "./components/TodoListDate";
 function TodoListItem() {
   return (
-    <div className="todo-list">
-      <div className="todo-list--date">January 5th 2023</div>
-      <div className="todo-list-info">
+    <div className="todo__list">
+      <TodoListDate/>
+      <div className="todo__list--info">
         <h2>Room rent.</h2>
-        <div className="todo-list--price">$423.43</div>
+        <div className="todo__list--price">$423.43</div>
       </div>
     </div>
   );
---
+++ Level 1 -- Components/todo-list/src/components/TodoListDate.jsx
@@ -0,0 +1,12 @@
+import "./TodoListDate.css";
+function TodoListDate() {
+  return (
+    <div className="todo__list--date">
+      <div className="todo__list--month">February</div>
+      <div className="todo__list--year"> 2023</div>
+      <div className="todo__list--day">12</div>
+    </div>
+  );
+}
+
+export default TodoListDate;
